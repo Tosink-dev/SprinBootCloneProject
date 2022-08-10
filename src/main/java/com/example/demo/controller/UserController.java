2ac425c8b5eeafedfb94b6f797cacb5593f6444f
Tosink-dev <110462935+Tosink-dev@users.noreply.github.com>
2022-08-10 13:15:07 +0100

    adding post mapping

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("/user")
 public class UserController {
 
-	
+	@GetMapping("/home")
 	public String home () {
 		
 		return "Welcome to HOME PAGE";
 	}
 	
+	@PostMapping("/dashboard")
 public String dashboard () {
 		
 		return "Welcome to DASHBOARD PAGE";
