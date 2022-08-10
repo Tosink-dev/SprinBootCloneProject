7fae4f32fd1b35ea975ad229e7a2ddec1ef2b253
Tosink-dev <110462935+Tosink-dev@users.noreply.github.com>
2022-08-10 13:07:49 +0100

    re-added user controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/user")
 public class UserController {
 
+	
+	public String home () {
+		
+		return "Welcome to HOME PAGE";
+	}
+	
+public String dashboard () {
+		
+		return "Welcome to DASHBOARD PAGE";
+	}
 }
